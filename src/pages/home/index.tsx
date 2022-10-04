4c26d8c6b139847bd5ef84c72401c847729d154e
Walter Palombini <walterpalombini@gmail.com>
2022-10-04 22:51:07 +1000

    New url

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -14,7 +14,7 @@ const HomePage = () => {
   return (
     <>
       <h1>Home Page</h1>
-      <button onClick={handleTest}>Test</button>
+      <button onClick={handleTest}>Test!</button>
     </>
   );
 };
