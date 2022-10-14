bb818a7d77a9205b78fdd54c20c409c4e3c25c69
Walter Palombini <walterpalombini@gmail.com>
2022-10-14 17:32:07 +1000

    isLoading and PrivateRoute functionality

--- src/containers/routes/index.tsx
+++ src/containers/routes/index.tsx
@@ -1,4 +1,4 @@
-import * as React from 'react';
+import React, { useEffect, lazy, useState } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import Layout from 'components/ui/layout/Layout';
@@ -12,22 +12,24 @@ import { getAuth } from 'firebase/auth';
 import { useAuthState } from 'react-firebase-hooks/auth';
 import { loginSuccessAction } from 'containers/account/actions';
 
-const AccountPage = React.lazy(() => import('pages/account'));
-const HomePage = React.lazy(() => import('pages/home'));
-const NotFoundPage = React.lazy(() => import('pages/not-found'));
-const PricingPage = React.lazy(() => import('pages/pricing'));
-const ProductsPage = React.lazy(() => import('pages/products'));
+const AccountPage = lazy(() => import('pages/account'));
+const HomePage = lazy(() => import('pages/home'));
+const NotFoundPage = lazy(() => import('pages/not-found'));
+const PricingPage = lazy(() => import('pages/pricing'));
+const ProductsPage = lazy(() => import('pages/products'));
 
 const AppRoutes = () => {
+  const [isLoading, setIsLoading] = useState(true);
   const [user, loading, error] = useAuthState(getAuth());
 
-  const { isAppLoaded } = useSelector((state: RootState) => ({
+  const { accountDetails, isAppLoaded } = useSelector((state: RootState) => ({
     isAppLoaded: state.route.isAppLoaded,
+    accountDetails: state.account,
   }));
 
   const dispatch = useDispatch();
 
-  React.useEffect(() => {
+  useEffect(() => {
     if (!isAppLoaded && !loading) {
       dispatch(appInitAction());
     }
@@ -36,6 +38,10 @@ const AppRoutes = () => {
       dispatch(loginSuccessAction());
     }
   }, [isAppLoaded, user, loading, error]);
+
+  useEffect(() => {
+    setIsLoading(Boolean(loading || accountDetails?.loading));
+  }, [loading, accountDetails]);
 
   if (!isAppLoaded) {
     return <LoadingPage />;
@@ -78,11 +84,15 @@ const AppRoutes = () => {
           <Route
             path="account"
             element={
-              <React.Suspense fallback={<LoadingPage />}>
-                <PrivateRoute>
-                  <AccountPage />
-                </PrivateRoute>
-              </React.Suspense>
+              isLoading ? (
+                <LoadingPage />
+              ) : (
+                <React.Suspense fallback={<LoadingPage />}>
+                  <PrivateRoute>
+                    <AccountPage />
+                  </PrivateRoute>
+                </React.Suspense>
+              )
             }
           />
           <Route
